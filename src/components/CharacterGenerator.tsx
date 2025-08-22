068ac1c32fd0bf6c51454dbddad47c854989a3fe
Marco Tondi <marco.tondi@exprivia.com>
2025-08-22 18:37:53 +0200

    fix: translation into Italian

--- src/components/CharacterGenerator.tsx
+++ src/components/CharacterGenerator.tsx
@@ -1,13 +1,11 @@
-'use client';
+"use client";
 
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { useState, useEffect } from "react";
-import {
-  generateCharacter,
-} from "@/lib/character-generator";
+import { generateCharacter } from "@/lib/character-generator";
 import { Character } from "@/lib/character-generator/types";
-import {useTranslations} from 'next-intl';
+import { useTranslations } from "next-intl";
 
 export default function CharacterGenerator() {
   const t = useTranslations();
@@ -28,8 +26,8 @@ export default function CharacterGenerator() {
 
   return (
     <div className="min-h-screen bg-background text-foreground font-body p-4 sm:p-6 md:p-8">
-      <main className="max-w-4xl mx-auto space-y-8">
-        <header className="text-center space-y-4 py-8">
+      <main className="max-w-4xl mx-auto space-y-6">
+        <header className="text-center space-y-2 py-2">
           <h1 className="text-3xl sm:text-4xl font-headline font-bold tracking-widest">
             <span className="text-primary relative">
               RŌNIN:
@@ -39,21 +37,21 @@ export default function CharacterGenerator() {
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
               >
-                <path
-                  d="M2 4.15625C18.5 2.15625 58.5 2.15625 98 6.15625"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
+              <path
+                d="M2 4.15625C18.5 2.15625 58.5 2.15625 98 6.15625"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
               </svg>
             </span>{" "}
-            {t('page.title')}
+            {t("page.title")}
           </h1>
         </header>
 
-        <div className="text-center space-y-4">
-          <p>{t('page.lastWarrior')}</p>
+        <div className="text-center space-y-1">
+          <p>{t("page.lastWarrior")}</p>
 
           <div className="flex justify-center gap-4">
             <Button
@@ -62,7 +60,7 @@ export default function CharacterGenerator() {
                 setLastDeath(lastDeath === "honour" ? null : "honour")
               }
             >
-              {t('page.honour')}
+              {t("page.honour")}
             </Button>
             <Button
               variant={lastDeath === "dishonour" ? "destructive" : "outline"}
@@ -70,27 +68,30 @@ export default function CharacterGenerator() {
                 setLastDeath(lastDeath === "dishonour" ? null : "dishonour")
               }
             >
-              {t('page.dishonour')}
+              {t("page.dishonour")}
             </Button>
-
           </div>
         </div>
 
-        <div className="flex justify-center items-center py-8">
+        <div className="flex justify-center items-center py-4">
           <button
             onClick={handleGenerate}
-            className="bg-transparent hover:bg-primary/10 text-foreground rounded-full border-2 border-dashed border-foreground/50 w-48 h-48 flex flex-col justify-center items-center text-center p-4 transition-colors duration-300"
+            className="bg-transparent hover:bg-primary/10 text-foreground rounded-full border-2 border-foreground/50 w-40 h-40 flex flex-col justify-center items-center text-center p-4 transition-colors duration-300"
           >
-            <span className="font-headline text-lg font-bold">{t('page.click')}</span>
-            <span className="text-sm">{t('page.forNew')}</span>
-            <span className="font-headline text-lg font-bold">{t('page.warrior')}</span>
+            <span className="font-headline text-lg font-bold">
+              {t("page.click")}
+            </span>
+            <span className="text-sm">{t("page.forNew")}</span>
+            <span className="font-headline text-lg font-bold">
+              {t("page.warrior")}
+            </span>
           </button>
         </div>
 
         {character && (
           <>
             <div className="text-center space-y-2">
-              <p className="text-muted-foreground">{t('page.youAre')}</p>
+              <p className="text-muted-foreground">{t("page.youAre")}</p>
               <h2 className="text-4xl font-headline font-bold text-primary">
                 {character.firstName.toUpperCase()}{" "}
                 {character.lastName.toUpperCase()}
@@ -104,7 +105,7 @@ export default function CharacterGenerator() {
               <p className="text-xl font-headline font-bold">
                 &quot;{character.nickName.japanese}&quot;
               </p>
-              <p className="text-muted-foreground">{t('page.the')}</p>
+              <p className="text-muted-foreground">{t("page.the")}</p>
               <h3 className="text-3xl font-headline font-bold text-primary/80 tracking-wider">
                 {t(character.class)}
               </h3>
@@ -114,18 +115,20 @@ export default function CharacterGenerator() {
 
             <div className="flex justify-around items-center text-center flex-wrap gap-4 text-sm sm:text-base">
               <p>
-                <span className="font-bold">{t('page.hp')}</span> <span className="">{character.hitPoints}</span>
+                <span className="font-bold">{t("page.hp")}</span>{" "}
+                <span className="">{character.hitPoints}</span>
               </p>
               <p>
-                <span className="font-bold">{t('page.virtues')}</span>{" "}
+                <span className="font-bold">{t("page.virtues")}</span>{" "}
                 {character.virtues} ({character.virtuesDice})
               </p>
               <p>
-                <span className="font-bold">{t('page.honor')}</span> {character.honor} (
-                {t(character.honorStatus)})
+                <span className="font-bold">{t("page.honor")}</span>{" "}
+                {character.honor} ({t(character.honorStatus)})
               </p>
               <p>
-                <span className="font-bold">{t('page.ryo')}</span> {character.ryo}
+                <span className="font-bold">{t("page.ryo")}</span>{" "}
+                {character.ryo}
               </p>
             </div>
 
@@ -134,7 +137,7 @@ export default function CharacterGenerator() {
             <div className="grid grid-cols-5 gap-x-8 gap-y-6">
               <div className="col-span-5 md:col-span-3 space-y-2">
                 <h3 className="text-2xl font-headline text-primary font-bold">
-                  {t('page.attributes')}
+                  {t("page.attributes")}
                 </h3>
                 <p className="text-muted-foreground">
                   {t(character.brokenBodies ?? "")}
@@ -145,13 +148,21 @@ export default function CharacterGenerator() {
               </div>
               <div className="col-span-5 md:col-span-2 space-y-2">
                 <h3 className="text-2xl font-headline text-primary font-bold">
-                  {t('page.abilities')}
+                  {t("page.abilities")}
                 </h3>
                 <ul className="list-disc list-inside space-y-1 pl-2 text-muted-foreground">
-                  <li>{t('page.swiftness')}: {character.abilities.swiftness}</li>
-                  <li>{t('page.spirit')}: {character.abilities.spirit}</li>
-                  <li>{t('page.vigor')}: {character.abilities.vigor}</li>
-                  <li>{t('page.resilience')}: {character.abilities.resilience}</li>
+                  <li>
+                    {t("page.swiftness")}: {character.abilities.swiftness}
+                  </li>
+                  <li>
+                    {t("page.spirit")}: {character.abilities.spirit}
+                  </li>
+                  <li>
+                    {t("page.vigor")}: {character.abilities.vigor}
+                  </li>
+                  <li>
+                    {t("page.resilience")}: {character.abilities.resilience}
+                  </li>
                 </ul>
               </div>
             </div>
@@ -159,12 +170,14 @@ export default function CharacterGenerator() {
             <Separator className="my-8" />
 
             <div className="text-center space-y-2">
-              <p className="text-muted-foreground">{t('page.youMustFollow')}</p>
+              <p className="text-muted-foreground">{t("page.youMustFollow")}</p>
               <h3 className="text-2xl font-headline font-bold text-primary tracking-wider">
                 {t(character.honourTenets.title)}
               </h3>
               <p className="text-muted-foreground">
-                {character.honourTenets.tenetsList.map((tenet: string) => t(tenet)).join(" • ")}
+                {character.honourTenets.tenetsList
+                  .map((tenet: string) => t(tenet))
+                  .join(" • ")}
               </p>
             </div>
 
@@ -173,7 +186,7 @@ export default function CharacterGenerator() {
             <div className="grid grid-cols-5 gap-x-8 gap-y-6">
               <div className="col-span-5 md:col-span-2 space-y-2">
                 <h3 className="text-2xl font-headline text-primary font-bold">
-                  {t('page.weapons')}
+                  {t("page.weapons")}
                 </h3>
                 <ul className="list-disc list-inside space-y-1 pl-2 text-muted-foreground">
                   {character.classWeapons.map((weapon, index) => (
@@ -181,21 +194,20 @@ export default function CharacterGenerator() {
                       <strong style={{ color: "#ECCF18" }}>
                         {t(weapon.name)}{" "}
                       </strong>
-                        {weapon.damage ? weapon.damage : ""}
-                        {" "}
-                        {weapon.amount ? t(weapon.amount) : ""}
+                      {weapon.damage ? weapon.damage : ""}{" "}
+                      {weapon.amount ? t(weapon.amount) : ""}
                     </li>
                   ))}
                 </ul>
               </div>
               <div className="col-span-5 md:col-span-3 space-y-2">
                 <h3 className="text-2xl font-headline text-primary font-bold">
-                  {t('page.armour')}
+                  {t("page.armour")}
                 </h3>
                 <div className="flex items-baseline gap-4">
                   <p className="font-semibold">{t(character.armor.style)}</p>
                   <p className="text-sm text-muted-foreground">
-                    {t('page.tier')} {character.armor.tier}
+                    {t("page.tier")} {character.armor.tier}
                   </p>
                 </div>
                 <p className="text-muted-foreground">
@@ -209,7 +221,7 @@ export default function CharacterGenerator() {
             <div className="grid grid-cols-5 gap-x-8 gap-y-6">
               <div className="col-span-5 md:col-span-2 space-y-2">
                 <h3 className="text-2xl font-headline text-primary font-bold">
-                  {t('page.equipment')}
+                  {t("page.equipment")}
                 </h3>
                 <ul className="list-disc list-inside space-y-1 pl-2 text-muted-foreground">
                   {character.equipment.map((item, index) => (
@@ -219,7 +231,7 @@ export default function CharacterGenerator() {
               </div>
               <div className="col-span-5 md:col-span-3 space-y-2">
                 <h3 className="text-2xl font-headline text-primary font-bold">
-                  {t('page.feature')}
+                  {t("page.feature")}
                 </h3>
                 <p className="font-semibold">{t(character.feature.title)}</p>
                 <p className="text-muted-foreground">
@@ -229,56 +241,56 @@ export default function CharacterGenerator() {
             </div>
 
             {(character.unseenText || character.shintaiText) && (
-            <>
-              <Separator className="my-8" />
-              <div className="py-8 flex justify-center">
-                <div className="flex w-full max-w-4xl items-start gap-8">
-                  {character.unseenText && (
-                    <div className="flex-1 space-y-4 text-center">
-                      <h3 className="text-2xl font-headline font-bold text-primary tracking-wider">
-                        {t('page.unseenText')}
-                      </h3>
-                      <p className="font-special text-muted-foreground text-sm tracking-[0.5em] bg-stone-700/50 rounded-sm px-4 py-1 inline-block">
-                        {t(character.unseenText.title)}
-                      </p>
-                      <p className="text-muted-foreground">
-                        {t(character.unseenText.description)}
-                      </p>
-                    </div>
-                  )}
+              <>
+                <Separator className="my-8" />
+                <div className="py-8 flex justify-center">
+                  <div className="flex w-full max-w-4xl items-start gap-8">
+                    {character.unseenText && (
+                      <div className="flex-1 space-y-4 text-center">
+                        <h3 className="text-2xl font-headline font-bold text-primary tracking-wider">
+                          {t("page.unseenText")}
+                        </h3>
+                        <p className="font-special text-muted-foreground text-sm tracking-[0.5em] bg-stone-700/50 rounded-sm px-4 py-1 inline-block">
+                          {t(character.unseenText.title)}
+                        </p>
+                        <p className="text-muted-foreground">
+                          {t(character.unseenText.description)}
+                        </p>
+                      </div>
+                    )}
 
-                  {/* Linea verticale di separazione */}
-                  {character.unseenText && character.shintaiText && (
-                    <div className="w-px bg-border h-auto self-stretch" />
-                  )}
+                    {/* Linea verticale di separazione */}
+                    {character.unseenText && character.shintaiText && (
+                      <div className="w-px bg-border h-auto self-stretch" />
+                    )}
 
-                  {character.shintaiText && (
-                    <div className="flex-1 space-y-4 text-center">
-                      <h3 className="text-2xl font-headline font-bold text-primary tracking-wider">
-                        {t('page.shintaiText')}
-                      </h3>
-                      <p className="font-special text-muted-foreground text-sm tracking-[0.5em] bg-stone-700/50 rounded-sm px-4 py-1 inline-block">
-                        {t(character.shintaiText.title)}
-                      </p>
-                      <p className="text-muted-foreground">
-                        {t(character.shintaiText.description)}
-                      </p>
-                    </div>
-                  )}
+                    {character.shintaiText && (
+                      <div className="flex-1 space-y-4 text-center">
+                        <h3 className="text-2xl font-headline font-bold text-primary tracking-wider">
+                          {t("page.shintaiText")}
+                        </h3>
+                        <p className="font-special text-muted-foreground text-sm tracking-[0.5em] bg-stone-700/50 rounded-sm px-4 py-1 inline-block">
+                          {t(character.shintaiText.title)}
+                        </p>
+                        <p className="text-muted-foreground">
+                          {t(character.shintaiText.description)}
+                        </p>
+                      </div>
+                    )}
+                  </div>
                 </div>
-              </div>
-            </>
-          )}
+              </>
+            )}
 
-
-            <div className="text-center pb-8">
+            {/* TODO: Abilitare pulsante download  */}
+            {/* <div className="text-center pb-8">
               <Button
                 size="lg"
                 className="font-headline font-bold tracking-wider"
               >
-                {t('page.download')}
+                {t("page.download")}
               </Button>
-            </div>
+            </div> */}
           </>
         )}
       </main>
